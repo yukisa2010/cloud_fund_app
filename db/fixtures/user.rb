989bf9b616f1799409296a2d84390d5ac2ed6e3b
USER <yukisa2010@gmail.com>
2023-02-23 15:21:19 +0900

    Userのfixtureを追加

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -2,5 +2,12 @@ User.seed do |s|
   s.id = 1
   s.email = 'admin@example.com'
   s.password = 'password'
+  s.status = :admin
+end
+
+User.seed do |s|
+  s.id = 2
+  s.email = 'user@example.com'
+  s.password = 'password'
   s.status = :normal
 end
