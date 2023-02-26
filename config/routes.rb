3269acc206cfdb0af8eb4cc55eb4754509696687
USER <yukisa2010@gmail.com>
2023-02-26 20:16:12 +0900

    users/deviseを整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/devise/sessions'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
