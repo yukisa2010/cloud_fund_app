50743eef5a1dbd69fa186e61668333f25eecfaec
USER <yukisa2010@gmail.com>
2023-03-02 13:17:54 +0900

    admin/projectsのrspecを修正

--- spec/requests/admin/projects_spec.rb
+++ spec/requests/admin/projects_spec.rb
@@ -51,5 +51,40 @@ RSpec.describe 'Admin::Projects', type: :request do
     end
   end
 
+  describe 'GET /admin/project/:id/edit' do
+    it 'アクセスできること' do
+      get edit_admin_project_path(project)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'PATCH /admin/projects/:id' do
+    let!(:user) { create(:user) }
+    let(:project) { create(:project, name: 'test project', user: user) }
+
+    context '正常系' do
+      let(:params) {
+        { project: { name: 'updated_name', description: 'description', user_id: user.id } }
+      }
+
+      it 'projectが作成されること' do
+        patch admin_project_path(project), params: params
+        expect(project.reload.name).to eq 'updated_name'
+        expect(response).to redirect_to edit_admin_project_path(project)
+      end
+    end
+
+    context '異常系' do
+      context 'invalidな値が送られた場合' do
+        let(:params) {
+          { project: { name: '', description: 'description', user_id: user.id } }
+        }
+
+        it 'projectが更新されないこと' do
+          patch admin_project_path(project), params: params
+          expect(project.reload.name).to eq 'test project'
+        end
+      end
+    end
   end
 end
