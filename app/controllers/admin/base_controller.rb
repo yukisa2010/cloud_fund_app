e6bab6aa44fb4da8136863f250a58359d5daa293
USER <yukisa2010@gmail.com>
2023-03-02 13:21:57 +0900

    view specを消去

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,7 @@ class Admin::BaseController < ApplicationController
   before_action :authenticate_user
 
   layout 'admin_layout'
+
   def authenticate_user
     redirect_to new_user_session_path unless current_user&.admin?
   end
